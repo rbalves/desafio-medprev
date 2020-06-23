cc1800c049c7f971f2367b238502a68ae3ecdcda
Rafael  Alves <rbalves.ads@gmail.com>
2020-06-23 13:37:07 -0400

    Add error API conditions

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseDomain = process.env.VUE_APP_URL_BASE;
-const baseURL = `${baseDomain}/ejrejgorejgo`;
+const baseURL = `${baseDomain}/box_da97eb998ae0f58a2f7e`;
 
 export default axios.create({
     baseURL,
